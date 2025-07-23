c3170741a83f1f1a2597d16bf689ce73d8de2d59
maxello <maxellort@gmail.com>
2025-07-23 23:51:12 +0300

    Changed footer text styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="py-5 bg-content1/80 border-t">
       <div className="container text-center font-medium">
-        <a target="_blank" href="https://github.com/maxello" className="underline">@maxello</a> 2025
+        <a target="_blank" href="https://github.com/maxello" className="underline text-secondary">@maxello</a> &#183; 2025
       </div>
     </div>
   )
